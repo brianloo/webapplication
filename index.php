ae634fa39ddd7e7b06292078535f07a1c0c5481e
Brian Loo <brian243026@dasein.edu.my>
2026-06-09 10:39:03 +0800

    week 1 library code completed

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+$servername = "localhost";
+$username = "brianloo";
+$password = "mDN@b_NanvTrDyW1";
+$dbname = "brianloo";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        * {
+            font-size: 20px;
+        }
+
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+        
+        </style>
+</head>
+<body>
+    <div id="email">
+        <form target="_self" method="POST">
+            <h2> Enter your Email: </h2>
+            <input type="text" name="email">
+            <br />
+            <h2> Password: </h2>
+            <input type="password" name="password">
+            <input type="submit">
+        </form>
+    </div>
+</body>
+</html>
